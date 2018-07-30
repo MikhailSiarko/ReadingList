98eb17f284a9f6b11fd6530d6070a06e454ca0cc
Mikhail Siarko <serko319@gmail.com>
2018-07-30 18:04:07 +0300

    Updates performed:  - Created demo compiled version of Update method for entities.

--- ReadingList.Domain/Infrastructure/Extensions/UpdateEntityExtension.cs
+++ ReadingList.Domain/Infrastructure/Extensions/UpdateEntityExtension.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using ReadingList.WriteModel;
 using ReadingList.WriteModel.Models.Base;
@@ -40,6 +42,65 @@ namespace ReadingList.Domain.Infrastructure.Extensions
                     entityProperty.SetValue(entity, sourceProperty.GetValue(source));
                 }
             }
+        }
+
+        public static void UpdateExpr<TEntity>(this TEntity entity, Dictionary<string, object> source) where TEntity : Entity
+        {
+            if(source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            if (!source.Any())
+                return;
+            
+            var entityProperties = entity.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public);
+            var properties = Expression.Constant(entityProperties);
+            
+            var n = Expression.Constant(entityProperties.Length);
+            var i = Expression.Parameter(typeof(int), "i");
+            var property = Expression.Parameter(typeof(PropertyInfo), "property");
+            var value = Expression.Parameter(typeof(object), "value");
+            var endLoop = Expression.Label();
+
+            var containsKeyMethodInfo =
+                source.GetType().GetMethod("ContainsKey", BindingFlags.Instance | BindingFlags.Public);
+
+            var anyMethodInfo = typeof(Enumerable).GetMethods()
+                .Single(m => m.Name == "Any" && m.GetParameters().Length == 2).MakeGenericMethod(typeof(CustomAttributeData));
+
+            var getValueOrDefaultMethodInfo = typeof(CollectionExtensions).GetMethods()
+                .Single(m => m.Name == "GetValueOrDefault" && m.GetParameters().Length == 2)
+                .MakeGenericMethod(typeof(string), typeof(object));
+
+            var setValueMethodInfo = typeof(PropertyInfo).GetMethod("SetValue", new[] {typeof(object), typeof(object)});
+
+            var entityParameter = Expression.Parameter(entity.GetType(), "entity");
+            var sourceParameter = Expression.Parameter(typeof(Dictionary<string, object>), "source");
+
+            Expression<Func<CustomAttributeData, bool>> predicate = a =>
+                a.AttributeType == typeof(IgnoreUpdateAttribute);
+
+            var block = Expression.Block(
+                new[] {i},
+                Expression.Assign(i, Expression.Constant(1)),
+                Expression.Loop(
+                    Expression.Block(
+                        new[] {property},
+                        Expression.IfThenElse(
+                            Expression.Not(Expression.LessThanOrEqual(i, Expression.Subtract(n, Expression.Constant(1)))),
+                            Expression.Break(endLoop),
+                            Expression.Assign(property, Expression.ArrayIndex(properties, i))),
+                        Expression.IfThen(
+                            Expression.Call(sourceParameter, containsKeyMethodInfo, Expression.Property(property, "Name")),
+                            Expression.Block(
+                                new[] {value},
+                                Expression.Assign(value, Expression.Call(getValueOrDefaultMethodInfo, sourceParameter, Expression.Property(property, "Name"))),
+                                Expression.Call(property, setValueMethodInfo, entityParameter, value))),
+                        Expression.PostIncrementAssign(i)), endLoop));
+            
+            // TODO Put it to the cache
+            var expr = Expression.Lambda<Action<TEntity, Dictionary<string, object>>>(block, entityParameter, sourceParameter).Compile();
+
+            expr(entity, source);
         }
     }
 }
---
+++ ReadingList.Tests/UpdateTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using ReadingList.Domain.Infrastructure.Extensions;
+using ReadingList.WriteModel.Models;
+using Xunit;
+
+namespace ReadingList.Tests
+{
+    public class UpdateTests
+    {
+        [Fact]
+        public void Update()
+        {
+            var bookList = new BookList
+            {
+                Name = "Default",
+                Type = BookListType.Private
+            };
+
+            var source = new Dictionary<string, object>
+            {
+                ["Name"] = "Updated",
+                ["Description"] = "Description"
+            };
+
+            bookList.UpdateExpr(source);
+
+            Assert.True(bookList.Name == (string)source["Name"]);
+        }
+    }
+}
