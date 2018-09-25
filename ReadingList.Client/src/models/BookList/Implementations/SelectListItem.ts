9ef0035b820069a5d9229c35aac50d39e12c5b18
Mikhail Siarko <serko319@gmail.com>
2018-09-25 16:20:54 +0300

    Added missing changes

--- ReadingList.Client/src/models/BookList/Implementations/SelectListItem.ts
+++ ReadingList.Client/src/models/BookList/Implementations/SelectListItem.ts
@@ -1,22 +1,4 @@
-export const BookStatus = {
-    1: 'To Reading',
-    2: 'Reading',
-    3: 'Started But Postponed',
-    4: 'Read'
-};
-
 export interface SelectListItem {
     text: string;
     value: string;
-}
-
-export function generateStatusSelectItems (): SelectListItem[] {
-    const options: SelectListItem[] = [];
-    for(let key in BookStatus) {
-        if(key) {
-            const text = BookStatus[key];
-            options.push({value: key, text});
-        }
-    }
-    return options;
 }
