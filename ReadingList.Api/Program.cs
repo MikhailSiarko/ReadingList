5b1000d453515e0e9746fd5ecf33be924cdb560a
Mikhail Siarko <serko319@gmail.com>
2018-10-19 13:33:48 +0300

    API: - Removed launching client project from code since it works only for Windows platform.

--- ReadingList.Api/Program.cs
+++ ReadingList.Api/Program.cs
@@ -13,13 +13,11 @@ namespace ReadingList.Api
     public class Program
     {
         private static Process _process;
-        private static bool _isDevelopment;
-        
+
         public static void Main(string[] args)
         {
             var host = BuildWebHost(args);
-            _isDevelopment = host.Services.GetRequiredService<IHostingEnvironment>().IsDevelopment();
-            
+
             using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
@@ -33,69 +31,11 @@ namespace ReadingList.Api
                     logger.LogError(ex, "An error occurred while seeding the database.");
                 }
             }
-            if(_isDevelopment)
-                LaunchClient();
-            
-            host.Run();
-            
-            if(_isDevelopment)
-                StopClient();
         }
 
         private static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
-
-        private static void ConfigureClientProcess()
-        {
-            _process = new Process
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    UseShellExecute = true,
-                    FileName = Environment.OSVersion.Platform == PlatformID.Win32NT
-                        ? Constants.CommandLine.Windows
-                        : Constants.CommandLine.Unix,
-                    WorkingDirectory = Path.Combine(
-                        Directory.GetParent(Directory.GetCurrentDirectory())
-                            .GetDirectories()
-                            .Single(x => x.Name == "ReadingList.Client").FullName),
-                    Arguments = (Environment.OSVersion.Platform == PlatformID.Win32NT
-                        ? Constants.CommandFlag.Windows
-                        : Constants.CommandFlag.Unix) + " npm run start"
-                }
-            };
-        }
-
-        private static void LaunchClient()
-        {
-            ConfigureClientProcess();
-            
-            _process.Start();
-        }
-
-        private static void StopClient()
-        {
-            _process.CloseMainWindow();
-            _process.Close();
-        }
-    }
-    
-    public static class Constants
-    {
-        public static class CommandLine
-        {
-            public const string Windows = "cmd";
-
-            public const string Unix = "#!/usr/bin/env bash";
-        }
-        
-        public static class CommandFlag
-        {
-            public const string Windows = "/c";
-
-            public const string Unix = "-c";
-        }
     }
 }
