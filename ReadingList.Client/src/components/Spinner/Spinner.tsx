8862d590cd777d17de99e30605455576bd0ccda3
Mikhail Siarko <serko319@gmail.com>
2019-02-08 19:04:29 +0300

    Added react-pose animation library and updated TypeScript version: - Added animation to the Spinner component.

--- ReadingList.Client/src/components/Spinner/Spinner.tsx
+++ ReadingList.Client/src/components/Spinner/Spinner.tsx
@@ -1,16 +1,42 @@
 import * as React from 'react';
+import { default as posed } from 'react-pose';
 import style from './Spinner.css';
 
 interface Props {
     loading: boolean;
 }
 
-const Spinner: React.SFC<Props> = props => (
-    <div
-        className={style['spinner-wrapper']} hidden={!props.loading}
-    >
-        <div className={style.spinner} />
-    </div>
-);
+const Animated = posed.div({
+    enter: {
+        opacity: 1,
+        zIndex: 999,
+        delay: 200,
+        transition: {
+            opacity: { ease: 'easeOut', duration: 500 },
+            zIndex: { ease: 'easeOut', duration: 600 }
+        }
+    },
+    exit: {
+        opacity: 0,
+        zIndex: -2,
+        delay: 200,
+        transition: {
+            opacity: { ease: 'easeOut', duration: 500 },
+            zIndex: { ease: 'easeOut', duration: 600 }
+        }
+    }
+});
+
+const Spinner: React.SFC<Props> = props => {
+    return (
+        props.loading ? (
+            <Animated
+                className={style['spinner-wrapper']}
+            >
+                <div className={style.spinner} />
+            </Animated>
+        ) : null
+    );
+};
 
 export default Spinner;
